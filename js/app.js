d43a32778ad262c4528009dbd68e9ab96d439b75
SoloDeveloperAlpha <112835682+SoloDeveloperAlpha@users.noreply.github.com>
2023-10-25 13:32:56 -0500

    CHanges on styles and app

--- js/app.js
+++ js/app.js
@@ -189,7 +189,7 @@ botonesHeader.forEach(boton => boton.addEventListener("click", (e) => {
                     cmain.style.backgroundImage="none";
                     mostrarPokemon(dataJSON);
                 } else {
-                    cmain.style.backgroundImage = `url("//assets/images/fondo${botonId}.png")`;
+                    cmain.style.backgroundImage = `url("/assets/images/fondo${botonId}.png")`;
                     const tipos = dataJSON.types.map(type => type.type.name);
                     if (tipos.some(tipo => tipo.includes(botonId))) {
                         mostrarPokemon(dataJSON);
