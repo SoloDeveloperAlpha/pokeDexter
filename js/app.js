e4735f2c1925764bbeed0406d8d602354a5373ae
SoloDeveloperAlpha <112835682+SoloDeveloperAlpha@users.noreply.github.com>
2024-06-23 09:39:47 -0500

    Update Pokedex

--- js/app.js
+++ js/app.js
@@ -1,54 +1,59 @@
-const listaPokemon = document.getElementById('listaPokemon');
+const listaPokemon = document.getElementById("listaPokemon");
 const botonesHeader = document.querySelectorAll(".btn-header");
-const cabecera = document.querySelector('header');
-const pokeData = document.getElementById('data');
-const cmain = document.querySelector('main');
+const cabecera = document.querySelector("header");
+const pokeData = document.getElementById("data");
+const cmain = document.querySelector("main");
 
-document.addEventListener("DOMContentLoaded", function() {
-    const yearSpan = document.getElementById("year");
-    const currentYear = new Date().getFullYear();
-    yearSpan.textContent = currentYear;
-    yearSpan.style.color="white";
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  const yearSpan = document.getElementById("year");
+  const currentYear = new Date().getFullYear();
+  yearSpan.textContent = currentYear;
+  yearSpan.style.color = "white";
+});
 
 import { num_min, num_max, gen } from "./generation.js";
 
 let url = "https://pokeapi.co/api/v2/pokemon/";
-let urlSpecie = "https://pokeapi.co/api/v2/pokemon-species/"
+let urlSpecie = "https://pokeapi.co/api/v2/pokemon-species/";
 gen.addEventListener("change", function () {
-  cabecera.style.backgroundColor="#f7f7f7";
-  cmain.style.backgroundImage="none";
-    listaPokemon.innerHTML = "";
-    pokeData.innerHTML = "";
-    pokeData.style.display = "none";
-    for (let i = num_min; i <= num_max; i++) {
-        fetch(url + i)
-            .then(data => {
-                return data.json();
-            })
-            .then(dataJSON => {
-                mostrarPokemon(dataJSON);
-            });
-    }
-
+  cmain.style.backgroundImage = "none";
+  listaPokemon.innerHTML = "";
+  pokeData.innerHTML = "";
+  pokeData.style.display = "none";
+  const promises = [];
+  for (let i = num_min; i <= num_max; i++) {
+    const promise = fetch(url + i)
+      .then((data) => data.json())
+      .then((dataJSON) => dataJSON)
+      .catch((error) => {
+        console.error("Ha ocurrido un ERROR", error);
+      });
+    promises.push(promise);
+  }
+  Promise.all(promises).then((results) => {
+    results.forEach((dataJSON) => {
+      mostrarPokemon(dataJSON);
+    });
+  });
 });
 
 function mostrarPokemon(data) {
-    let tipos = data.types.map((type) =>
-        `<p class="${type.type.name} tipo">${type.type.name}</p>`);
-    tipos = tipos.join('');
-    let pokeId = data.id.toString();
-    if (pokeId.length === 1) {
-        pokeId = "00" + pokeId;
-    } else if (pokeId.length === 2) {
-        pokeId = "0" + pokeId;
-    }
+  let tipos = data.types.map(
+    (type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`
+  );
+  tipos = tipos.join("");
+  let pokeId = data.id.toString();
+  if (pokeId.length === 1) {
+    pokeId = "00" + pokeId;
+  } else if (pokeId.length === 2) {
+    pokeId = "0" + pokeId;
+  }
 
-    const div = document.createElement("div");
-    const altura = data.height / 10;
-    const peso = data.weight / 10;
-    div.classList.add("pokemon");
-    div.innerHTML = `<p class="pokemon-id-back">#${pokeId}</p>
+  const div = document.createElement("div");
+  const altura = data.height / 10;
+  const peso = data.weight / 10;
+  div.classList.add("pokemon");
+  div.innerHTML = `<p class="pokemon-id-back">#${pokeId}</p>
                     <div class="pokemon-imagen">
                         <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}" srcset="">
                     </div>
@@ -64,38 +69,42 @@ function mostrarPokemon(data) {
                             <p class="stat">${altura}M</p>
                             <p class="stat">${peso}KG</p>
                         </div>
-                    </div>`
-    listaPokemon.append(div);
+                    </div>`;
+  listaPokemon.append(div);
 
-    div.addEventListener("click", (e) => {
-        listaPokemon.innerHTML = "";
-        //pantallaEspera();
-        pokeData.innerHTML = "";
-        pokeData.style.display = "grid";
-        let pokeInfo = e.currentTarget;
-        const img_poke = pokeInfo.querySelector('.pokemon-imagen img');
-        let nom_poke = pokeInfo.querySelector('.pokemon-info h2');
+  div.addEventListener("click", (e) => {
+    listaPokemon.innerHTML = "";
+    //pantallaEspera();
+    pokeData.innerHTML = "";
+    pokeData.style.display = "grid";
+    let pokeInfo = e.currentTarget;
+    const img_poke = pokeInfo.querySelector(".pokemon-imagen img");
+    let nom_poke = pokeInfo.querySelector(".pokemon-info h2");
 
-        if (nom_poke) { nom_poke.textContent = nom_poke.textContent.toUpperCase(); }
+    if (nom_poke) {
+      nom_poke.textContent = nom_poke.textContent.toUpperCase();
+    }
 
-        let pTipo = data.types.map((type) =>
+    let pTipo = data.types.map(
+      (type) =>
         `<div class="poke-type ${type.type.name}">
         ${type.type.name.toUpperCase()}
-        </div>`);
-        pTipo = pTipo .join('');
+        </div>`
+    );
+    pTipo = pTipo.join("");
 
-        //PokeData contiene a Dexter, DexterData, DexterHab y DexterBotones
-        const dexter = document.createElement('div');
-        dexter.classList.add("dexter");
-        const dexterData = document.createElement('div');
-        dexterData.classList.add("dexter-data");
-        const dexterPokeName = document.createElement('div');//Dentro de DexterData
-        dexterPokeName.classList.add("dexter-poke-name");
-        
-        async function obtenerEspec() {
-          try {
-              var es_p = await pokemonEspecie(data.id);
-              dexterData.innerHTML = `
+    //PokeData contiene a Dexter, DexterData, DexterHab y DexterBotones
+    const dexter = document.createElement("div");
+    dexter.classList.add("dexter");
+    const dexterData = document.createElement("div");
+    dexterData.classList.add("dexter-data");
+    const dexterPokeName = document.createElement("div"); //Dentro de DexterData
+    dexterPokeName.classList.add("dexter-poke-name");
+
+    async function obtenerEspec() {
+      try {
+        var es_p = await pokemonEspecie(data.id);
+        dexterData.innerHTML = `
                             <div class="dexter-poke-name">
                               <div id="poke-name1">
                               <img src="assets/images/pokebola.png" alt="" srcset="">${pokeId} ${nom_poke.textContent}</div>
@@ -111,99 +120,102 @@ function mostrarPokemon(data) {
                             <div class="dexter-poke-info">
                                 <p>${es_p.valor2}</p>
                             </div>`;
-          } catch (error) {
-              console.error("Error al obtener la descripción:", error);
-          }
-          
-        }
-        obtenerEspec();//Se pone DesterPokeData dentro de DexterData
-        
-        pokeData.append(dexter);
-        pokeData.append(dexterData);
-        dexter.append(img_poke);
+      } catch (error) {
+        console.error("Error al obtener la descripción:", error);
+      }
+    }
+    obtenerEspec(); //Se pone DesterPokeData dentro de DexterData
 
-    });
+    pokeData.append(dexter);
+    pokeData.append(dexterData);
+    dexter.append(img_poke);
+  });
 }
 
 async function pokemonEspecie(pID) {
-    try {
-        const response = await fetch(urlSpecie + pID);
-        const dataJSON = await response.json();
-        const { genera } = dataJSON;
-        const { flavor_text_entries } = dataJSON;
-        const { moves } = dataJSON;
-        const genus = genera[5].genus;
-        for(let x=0;x<=flavor_text_entries.length;x++){
-          let lang=flavor_text_entries[x].language;
-          if(lang.name==='es'){
-            let det_esp=flavor_text_entries[x].flavor_text;
-            return {valor1:genus,valor2:det_esp,valor3:moves};
-          }
-        }
-    } catch (error) {
-        console.error("Error al obtener la especie del Pokémon:", error);
-        throw error;
+  try {
+    const response = await fetch(urlSpecie + pID);
+    const dataJSON = await response.json();
+    const { genera } = dataJSON;
+    const { flavor_text_entries } = dataJSON;
+    const { moves } = dataJSON;
+    const genus = genera[5].genus;
+    for (let x = 0; x <= flavor_text_entries.length; x++) {
+      let lang = flavor_text_entries[x].language;
+      if (lang.name === "es") {
+        let det_esp = flavor_text_entries[x].flavor_text;
+        return { valor1: genus, valor2: det_esp, valor3: moves };
+      }
     }
+  } catch (error) {
+    console.error("Error al obtener la especie del Pokémon:", error);
+    throw error;
+  }
 }
 
 async function obtenerPokemon(pkID) {
   try {
-      const response = await fetch(url + pkID);
-      const dataJSON = await response.json();
-      const { moves } = dataJSON;
-      return {val1:moves};
+    const response = await fetch(url + pkID);
+    const dataJSON = await response.json();
+    const { moves } = dataJSON;
+    return { val1: moves };
   } catch (error) {
-      console.error("Error al obtener DATOS Pokémon:", error);
-      throw error;
+    console.error("Error al obtener DATOS Pokémon:", error);
+    throw error;
   }
 }
 
-async function nuevoAttak(Rnd,atk){
-  try{
-      while(Rnd.length<4){
-        const RndAl = Math.floor(Math.random()* atk+1);
-        if(!Rnd.includes(RndAl)){
-          Rnd.push(RndAl);
-        }
+async function nuevoAttak(Rnd, atk) {
+  try {
+    while (Rnd.length < 4) {
+      const RndAl = Math.floor(Math.random() * atk + 1);
+      if (!Rnd.includes(RndAl)) {
+        Rnd.push(RndAl);
       }
-      return Rnd;
-  }catch(error){
+    }
+    return Rnd;
+  } catch (error) {
     console.error("Error al obtener RANDOM ATAQUES Pokémon:", error);
-      throw error;
+    throw error;
   }
 }
 
-botonesHeader.forEach(boton => boton.addEventListener("click", (e) => {
+botonesHeader.forEach((boton) =>
+  boton.addEventListener("click", (e) => {
     const botonId = e.currentTarget.id;
     let estilos = window.getComputedStyle(document.getElementById(botonId));
     listaPokemon.innerHTML = "";
     for (let i = num_min; i <= num_max; i++) {
-        fetch(url + i)
-            .then(data => {
-                return data.json();
-            })
-            .then(dataJSON => {
-                pokeData.innerHTML = "";
-                pokeData.style.display = "none";
-                if (botonId === "ver-todos") {
-                    cmain.style.backgroundImage="none";
-                    mostrarPokemon(dataJSON);
-                } else {
-                    let fondo=`fondo${botonId}.png`;
-                    cmain.style.backgroundImage = `url(./assets/images/${fondo})`;
-                    const tipos = dataJSON.types.map(type => type.type.name);
-                    if (tipos.some(tipo => tipo.includes(botonId))) {
-                        mostrarPokemon(dataJSON);
-                    }
-                }
-            });
+      fetch(url + i)
+        .then((data) => {
+          return data.json();
+        })
+        .then((dataJSON) => {
+          pokeData.innerHTML = "";
+          pokeData.style.display = "none";
+          if (botonId === "ver-todos") {
+            cmain.style.backgroundImage = "none";
+            mostrarPokemon(dataJSON);
+          } else {
+            let fondo = `fondo${botonId}.png`;
+            cmain.style.backgroundImage = `url(./assets/images/${fondo})`;
+            const tipos = dataJSON.types.map((type) => type.type.name);
+            if (tipos.some((tipo) => tipo.includes(botonId))) {
+              mostrarPokemon(dataJSON);
+            }
+          }
+        });
     }
 
-    let valorPropiedad = estilos.getPropertyValue('background-color');
-    cabecera.style.backgroundColor = `${valorPropiedad}`;
-}));
+    /*if (botonId === "ver-todos") {
+      cabecera.style.backgroundColor = `#dd1035`;
+    } else {
+      let valorPropiedad = estilos.getPropertyValue("background-color");
+      cabecera.style.backgroundColor = `${valorPropiedad}`;
+    }*/
+  })
+);
 
 /*function pantallaEspera(){
 
 }*/
-
